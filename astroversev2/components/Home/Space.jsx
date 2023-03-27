28d69064a4f53fa2b957949b40f5698736d8f3f5
fauzansyll <fauzansyll@gmail.com>
2023-03-27 21:38:11 +0700

    First init

---
+++ astroversev2/components/Home/Space.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Link from "next/link";
+import Image from "next/image";
+import style from '@/styles/space.module.css'
+
+const Space = () => {
+    return(
+        <div className={`${style.space}`}>
+            <p>Test</p>
+        </div>
+    )
+};
+
+export default Space;
+
